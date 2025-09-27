56060ae9543c9d960f7c1bff5638de20fddfcd4e
snowmiku <1352218398a@gmail.com>
2025-09-27 00:40:19 +0000

    FINALIZE: Remove nested gitignores and commit full source tree

---
+++ fontconfig-2.16.0/src/fcstdint.h
@@ -0,0 +1,9 @@
+#ifndef _FONTCONFIG_SRC_FCSTDINT_H
+#define _FONTCONFIG_SRC_FCSTDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "fontconfig 2.16.0"
+/* generated using gnu compiler arm-rockchip830-linux-uclibcgnueabihf-gcc (crosstool-NG 1.24.0) 8.3.0 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
